31b6dc85061df65a6ea36cb9a0436c52d481f163
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-15 09:03:51 +0400

    create  VisitorService

---
+++ visitorlog/src/main/java/com/example/visitorlog/Service/VisitorService.java
@@ -0,0 +1,4 @@
+package com.example.visitorlog.Service;
+
+public class VisitorService {
+}
--- visitorlog/src/main/java/com/example/visitorlog/model/Visitor.java
+++ visitorlog/src/main/java/com/example/visitorlog/model/Visitor.java
@@ -1,8 +1,12 @@
 package com.example.visitorlog.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Visitor {
     private Long id;
     private String name;
