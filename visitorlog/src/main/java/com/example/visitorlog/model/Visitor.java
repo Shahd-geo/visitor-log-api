803762ae74a4f3a3b4ee22db1c0edc5515f00575
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-15 09:00:44 +0400

    add Date notaion to create the getter and setter

--- visitorlog/src/main/java/com/example/visitorlog/model/Visitor.java
+++ visitorlog/src/main/java/com/example/visitorlog/model/Visitor.java
@@ -1,4 +1,11 @@
 package com.example.visitorlog.model;
 
+import lombok.Data;
+
+@Data
 public class Visitor {
+    private Long id;
+    private String name;
+    private String company;
+    private String purpose;
 }
